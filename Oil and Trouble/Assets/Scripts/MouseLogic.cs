c5f15d9b97d798f21d161f03cb2723344fd12e14
Matthew Gonzales <matthekg@uci.edu>
2019-04-19 23:22:26 -0700

    Basic Player Movement and Status
    +Added Player multi jumps
    +Added UI for health and splitscreen
    +Added player health and healing buttong

---
+++ Oil and Trouble/Assets/Scripts/MouseLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class MouseLogic : MonoBehaviour
+{
+
+    [Header("Button Values")]
+    public int healingAmount = 1;
+    private bool isHealPressed;
+
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    void Update()
+    {
+        if (isHealPressed)
+            GameObject.FindWithTag("Player").gameObject.GetComponent<RobotStatus>().HealSelf(healingAmount);
+    }
+
+    public void onPointerDownHealing()
+    {
+        isHealPressed = true;
+    }
+    public void onPointerUpHealing()
+    {
+        isHealPressed = false;
+    }
+}
---
+++ Oil and Trouble/Assets/Scripts/RobotStatus.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobotStatus : MonoBehaviour
+{
+    [Header("HealthUI")]
+    [SerializeField] float maxHealth;
+    [SerializeField] float health;
+    private Slider slider;
+
+    void Awake()
+    {
+        health = maxHealth;
+        slider = GameObject.Find("HealthBar").GetComponent<Slider>();
+    }
+    void Start()
+    {
+        UpdateHealthBar();
+    }
+    void Update()
+    {
+        if (health <= 0) //game over
+            Time.timeScale = 0;
+
+        if (Input.GetKey("o"))
+            DamageSelf(1);
+    }
+
+    private void UpdateHealthBar()
+    {
+        slider.value = health;
+    }
+
+    public void DamageSelf(float damage)
+    {
+        health -= damage;
+        UpdateHealthBar();
+    }
+
+    public void HealSelf(float healing)
+    {
+        health += healing;
+        if (health > maxHealth)
+            health = maxHealth;
+        UpdateHealthBar();
+    }
+}
