47718997e459374a826179b2e625ebf31983407b
andrewabenoja <aabenoja@uci.edu>
2019-05-02 00:30:45 -0700

    Enemy Jump Detection Hitbox

---
+++ Oil and Trouble/Assets/Scripts/EnemyJump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJump : MonoBehaviour
+{
+    public float xThrust;
+    public float yThrust;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Enemy"))
+        {
+            collider.GetComponent<Rigidbody2D>().AddForce(new Vector2(xThrust, yThrust), ForceMode2D.Impulse);
+        }
+    }
+}
